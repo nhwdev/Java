cabd8c21aa2e4f882c0c2f75e5001bb51b548840
shtu0 <shtu0@DESKTOP-QHA1IK6>
2026-02-02 13:27:25 +0900

    test git 1

--- codingTest/src/boj/boj_11.java
+++ codingTest/src/boj/boj_11.java
@@ -1,10 +1,16 @@
 package boj;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class boj_11 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("HelloWorld");
+		bw.flush();
+		bw.close();
 	}
 
 }
