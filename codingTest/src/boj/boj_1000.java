4887ea1c58b47466510ab0fb232f82f31733361e
shtu0 <shtu0@DESKTOP-QHA1IK6>
2026-02-02 14:52:50 +0900

    Baekjoon

--- codingTest/src/boj/boj_1000.java
+++ codingTest/src/boj/boj_1000.java
@@ -12,7 +12,7 @@ public class boj_1000 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        bw.write(Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken()) + "");
+        bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "");
         bw.flush();
         bw.close();
 	}
