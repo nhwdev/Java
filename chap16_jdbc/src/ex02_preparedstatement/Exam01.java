f202e0fe4c2c7f40ed19dd166a750f239ea350da
nhwdev <nhw.dev@gmail.com>
2026-02-05 09:29:14 +0900

    Java Database Connectivity

--- chap16_jdbc/src/ex02_preparedstatement/Exam01.java
+++ chap16_jdbc/src/ex02_preparedstatement/Exam01.java
@@ -14,26 +14,25 @@ public class Exam01 {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName("org.mariadb.jdbc.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gdjdb", "gduser", "4986");
-		PreparedStatement pstmt1 = conn.prepareStatement
-				("select distinct deptno from professor where deptno is not null");
-		PreparedStatement pstmt2 = conn.prepareStatement
-				("select * from professor where deptno = ?");
+		// Statement > PreparedStatement : SQL구문실행객체
+		PreparedStatement pstmt1 = conn.prepareStatement // 중복이 없는 NULL이 아닌 "deptno"의 값을 호출
+		("select distinct deptno from professor where deptno is not null");
+		PreparedStatement pstmt2 = conn.prepareStatement("select * from professor where deptno = ?");
 		ResultSet rs1 = pstmt1.executeQuery();
-		while(rs1.next()) {
-			int deptno = rs1.getInt(1);
-			pstmt2.setInt(1, deptno);
+		while (rs1.next()) {
+			int deptno = rs1.getInt(1); // 부서코드 1개
+			pstmt2.setInt(1, deptno); // 1번째 '?'에 값을 설정
 			System.out.println("=====" + deptno);
 			ResultSet rs2 = pstmt2.executeQuery();
-			while(rs2.next()) {
-				System.out.printf("교수번호: %s, 이름: %s, 직급: %s, 급여: %d, 보너스: %d, 부서코드: %d, 입사일: %s\n",
-						rs2.getString("no"),
+			while (rs2.next()) {
+				System.out.printf("교수번호: %s, 이름: %s, 직급: %s, 급여: %d, 보너스: %d, 부서코드: %d, 입사일: %s\n", rs2.getString("no"),
 						rs2.getString("name"),
 						rs2.getString("position"),
 						rs2.getInt("salary"),
 						rs2.getInt("bonus"),
 						rs2.getInt("deptno"),
 						rs2.getDate("hiredate"));
-		}		
+			}
+		}
 	}
-}
 }
