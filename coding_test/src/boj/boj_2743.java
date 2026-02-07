13d66d4d5039c0c93e5201d6ba15918466b79baa
nhwdev <nhw.dev@gmail.com>
2026-02-07 15:00:58 +0900

    https://www.acmicpc.net/problem/2743

---
+++ coding_test/src/boj/boj_2743.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// https://www.acmicpc.net/problem/2743
+public class boj_2743 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(String.valueOf(br.readLine().length()));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
