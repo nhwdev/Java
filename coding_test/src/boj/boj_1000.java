a29c432ecad62281babf8dd41de54fa08b977c53
nhwdev <nhw.dev@gmail.com>
2026-02-07 14:57:14 +0900

    https://www.acmicpc.net/problem/1000

---
+++ coding_test/src/boj/boj_1000.java
@@ -0,0 +1,20 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/1000
+public class boj_1000 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "");
+        bw.flush();
+        bw.close();
+	}
+}
