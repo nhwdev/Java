8fb54a87d665176e613ab275bf050980b497712f
nhwdev <nhw.dev@gmail.com>
2026-02-10 17:41:10 +0900

    https://www.acmicpc.net/problem/1152

---
+++ coding_test/src/boj/boj_1152.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// https://www.acmicpc.net/problem/1152
+// 예제 입력 1 
+// The Curious Case of Benjamin Button
+// 예제 출력 1 
+// 6
+public class boj_1152 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] str = br.readLine().trim().split(" ");
+//		int length = str.length;
+//		for(int i=0; i< str.length; i++) {
+//			if(str[i].equals("")){
+//				length--;
+//			}
+//		}
+		bw.write(String.valueOf(str.length));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
