cec1df77255125e6e81b14590cf2285b27311622
nhwdev <nhw.dev@gmail.com>
2026-02-25 16:33:10 +0900

    https://www.acmicpc.net/problem/2745

---
+++ coding_test/src/boj/BOJ_2745.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2745
+public class BOJ_2745 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw.write(String.valueOf(Integer.parseInt(st.nextToken(), Integer.parseInt(st.nextToken()))));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
