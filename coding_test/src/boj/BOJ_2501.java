416a07c716bc5b13c0950d541f9ac02cccc81716
nhwdev <nhw.dev@gmail.com>
2026-03-05 08:59:36 +0900

    https://www.acmicpc.net/problem/2581

---
+++ coding_test/src/boj/BOJ_2501.java
@@ -0,0 +1,33 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2501
+public class BOJ_2501 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int count = 0;
+		int number = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				count++;
+				if (count == k) {
+					number = i;
+				} 
+			}
+		}
+		bw.write(String.valueOf(number));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
